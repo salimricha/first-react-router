78544b5234fe8d3c447d103557c87061419e8f3e
salimricha <salimricha12@gmail.com>
2021-05-19 12:12:55 +0300

    Added page2.jsx

--- react-router/src/Routes.js
+++ react-router/src/Routes.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./App";
+import Page2 from "./Page2";
 import Profile from "./Profile";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/" component={App} />
+      <Route exact path="/" component={App} />
+      <Route exact path="/page2" component={Page2} /> 
+      <Route exact path="/profile" component={Profile} />
+        
       </Switch>
     </BrowserRouter>
   );
